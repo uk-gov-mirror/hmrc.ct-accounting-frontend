8f714fc0398c18c74b08241801239819bed5a9cd
jowensoc <114502866+jowensoc@users.noreply.github.com>
2026-09-23 17:19:19 +0100

    DTR-6411 Rebased main and resolved conflicts

--- app/viewmodels/PageBreadcrumbs.scala
+++ app/viewmodels/PageBreadcrumbs.scala
@@ -106,4 +106,13 @@ object PageBreadcrumbs {
       )
     )
   )
+
+  def repaymentsReallocationsPage(implicit messages: Messages): Breadcrumbs = Breadcrumbs(
+    // TODO: Add hrefs
+    items = Seq(
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.home")), href = Some("/")),
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriods")), href = Some("/")),
+      BreadcrumbsItem(content = Text(messages("breadcrumbs.accountingPeriodEnding")), href = Some("/"))
+    )
+  )
 }
